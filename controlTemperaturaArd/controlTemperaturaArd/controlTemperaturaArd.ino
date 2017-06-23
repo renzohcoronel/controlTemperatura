7dd550766104bc51cbbb489bed6f579c2cc72760
Renzo H Coronel <renzo.h.coronel@gmail.com>
2017-06-22 22:16:52 -0300

    * Corregido la conversion de char* a char[] para el json

--- controlTemperaturaArd/controlTemperaturaArd/controlTemperaturaArd.ino
+++ controlTemperaturaArd/controlTemperaturaArd/controlTemperaturaArd.ino
@@ -1,40 +1,55 @@
-#include <ArduinoJson.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ArduinoJson.h>
 #include <tempo.h>
+#include <EEPROM.h>
+
 
 //Relays
 #define RELAY1 2
-#define RELAY1 3
-#define RELAY1 4
+#define RELAY2 3
+#define RELAY3 4
 
 #define HISTERESIS 0.6
 
 //Sensores Temperatura
 #define BUSONEWIRE1 6
-#define BUSONEWIRE1 7
-#define BUSONEWIRE1 8
+#define BUSONEWIRE2 7
+#define BUSONEWIRE3 8
 
 OneWire oneWire1(BUSONEWIRE1);
 DallasTemperature sensor1(&oneWire1);
 
-OneWire oneWire1(BUSONEWIRE1);
+OneWire oneWire2(BUSONEWIRE2);
 DallasTemperature sensor2(&oneWire2);
 
-OneWire oneWire1(BUSONEWIRE1);
+OneWire oneWire3(BUSONEWIRE3);
 DallasTemperature sensor3(&oneWire3);
 
-Tempo t_temp(30*1000); // temporizador para la lectura de temperatura
+Tempo t_temp(2*1000); // temporizador para la lectura de temperatura
 
 //---Agregado Renzo
 
 struct SensorSetting   //generamos la estructura point para poder guardar la posicion de los motores en cada punto.
 {
-  int  sensor=0;   //pulsos motor joint 1
-  int  tempSet= 0;
+  const int  sensor1;  
+  char descripcion1[20];
+  float valor1;
+  float  valorSet1;
+
+  const int  sensor2;  
+  char descripcion2[20];
+  float valor2;
+  float  valorSet2;
+
+  const int  sensor3;  
+  char descripcion3[20];
+  float valor3;
+  float  valorSet3;
 };
 
-int eeAdd = 0;
+
+SensorSetting sensorSetting = {1,"Sensor1",0,10, 2,"Sensor2",0,15, 3,"Sensor3",0,20 };
 
 //-----------------
 
@@ -45,58 +60,117 @@ void setup() {
   pinMode(RELAY2,OUTPUT);
   pinMode(RELAY3,OUTPUT);
 
-  
+  EEPROM.put(0,sensorSetting);
+  EEPROM.get(0,sensorSetting);
 
+ 
 }
 
 void loop() {
-
+  
+    StaticJsonBuffer<250> jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+    
    if (t_temp.state())
   {
-    //Sensor 1
+    //----------------Sensor 1---------------------------
         sensor1.requestTemperatures();
-        float temp1=sensor1.getTempCByIndex(0);
+        sensorSetting.valor1 = sensor1.getTempCByIndex(0);
         
-        if(temp1 > tempSet1+HISTERESIS){
+        if(sensorSetting.valor1 > sensorSetting.valorSet1+HISTERESIS){
           digitalWrite(RELAY1,HIGH);
-          Serial.print("ON  ");
-          Serial.println(temp1);
+       
          } else{
-            if ( temp1<tempSet1-HISTERESIS){  
-              Serial.print("OFF  ");
-              Serial.println(temp1);
-              digitalWrite(RELAY1,LOW);
+            if ( sensorSetting.valor1 <sensorSetting.valorSet1-HISTERESIS){  
+                  digitalWrite(RELAY1,LOW);
+         
               }
           }  
-    //Sensor 2
+          
+    JsonObject& jsonObjectSensor1 = root.createNestedObject("");
+    jsonObjectSensor1["id"] = sensorSetting.sensor1;
+    jsonObjectSensor1["descripcion"] = sensorSetting.descripcion1;
+    jsonObjectSensor1["valor"] = sensorSetting.valor1;
+    jsonObjectSensor1["valorSet"] = sensorSetting.valorSet1;
+          
+    //----------------Sensor 2---------------------------
       sensor2.requestTemperatures();
-      float temp2=sensor2.getTempCByIndex(0);
+      sensorSetting.valor2 = sensor2.getTempCByIndex(0);
         
-      if(temp2 > tempSet2+HISTERESIS){
+      if(sensorSetting.valor2 > sensorSetting.valorSet2+HISTERESIS){
          digitalWrite(RELAY2,HIGH);
-         Serial.print("ON  ");
-         Serial.println(temp2);
+ 
        } else{
-          if ( temp2<tempSet2-HISTERESIS){  
-            Serial.print("OFF  ");
-            Serial.println(temp2);
-            digitalWrite(RELAY2,LOW);
-            }
-       }  
-      //Sensor 3
-      sensor3.requestTemperatures();
-      float temp3=sensor3.getTempCByIndex(0);
-        
-      if(temp3 > tempSet3+HISTERESIS){
-         digitalWrite(RELAY2,HIGH);
-         Serial.print("ON  ");
-         Serial.println(temp2);
-       } else{
-          if ( temp1<tempSet1-HISTERESIS){  
-            Serial.print("OFF  ");
-            Serial.println(temp3);
-            digitalWrite(RELAY3,LOW);
+          if ( sensorSetting.valor2 < sensorSetting.valorSet2-HISTERESIS){  
+               digitalWrite(RELAY2,LOW);
             }
        } 
+
+    JsonObject& jsonObjectSensor2 = root.createNestedObject("");
+    jsonObjectSensor2["id"] = sensorSetting.sensor2;
+    jsonObjectSensor2["descripcion"] = sensorSetting.descripcion2;
+    jsonObjectSensor2["valor"] = sensorSetting.valor2;
+    jsonObjectSensor2["valorSet"] = sensorSetting.valorSet2;
+    
+     //----------------Sensor 3---------------------------
+      sensor3.requestTemperatures();
+      sensorSetting.valor3 = sensor3.getTempCByIndex(0);
+        
+      if(sensorSetting.valor3 > sensorSetting.valorSet3+HISTERESIS){
+         digitalWrite(RELAY3,HIGH);
+    
+       } else{
+          if ( sensorSetting.valor3 < sensorSetting.valorSet3-HISTERESIS){  
+               digitalWrite(RELAY3,LOW);
+            }
+       }
+
+    JsonObject& jsonObjectSensor3 = root.createNestedObject("");
+    jsonObjectSensor3["id"] = sensorSetting.sensor3;
+    jsonObjectSensor3["descripcion"] = sensorSetting.descripcion3;
+    jsonObjectSensor3["valor"] = sensorSetting.valor3;
+    jsonObjectSensor3["valorSet"] = sensorSetting.valorSet3;
+
+    root.printTo(Serial);
+    
   }
+
+  if(Serial.available())
+  {
+      String jsonString = "";
+      char character;
+
+      while(Serial.available()) {
+        character = Serial.read();
+        jsonString.concat(character);
+     }
+
+     StaticJsonBuffer<200> jsonBuffer;
+     JsonObject& jsonData = jsonBuffer.parseObject(jsonString);
+
+     if (!jsonData.success())
+     {
+        Serial.println("parseObject() failed");
+     }
+
+     int idSensor = jsonData["id"];
+     switch (idSensor)
+     {
+        case 1:
+              strncpy(sensorSetting.descripcion1,jsonData["descripcion"],18);
+              sensorSetting.valorSet1 = jsonData["valorSet"];
+           break;
+        case 2:
+            strncpy(sensorSetting.descripcion2,jsonData["descripcion"],18);
+            sensorSetting.valorSet2 = jsonData["valorSet"];
+           break;
+        case 3:
+            strncpy(sensorSetting.descripcion3,jsonData["descripcion"],18);
+            sensorSetting.valorSet3 = jsonData["valorSet"];
+           break;
+        default:
+              break;            
+     }
+  }
+  
 }
